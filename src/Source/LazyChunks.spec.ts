3831daa717b63a388e1c5d7c476d8d7e97dfe7f8
Frank Hampus Weslien <frankhampusweslien@google.com>
2023-08-14 09:26:07 +0200

    Add failing test for Lazy Chunk

--- src/Source/LazyChunks.spec.ts
+++ src/Source/LazyChunks.spec.ts
@@ -4,6 +4,10 @@ import PullStream from "./PullStream.js";
 import LazeChunks from "./LazyChunks.js";
 import * as Assert from "./Assert.js";
 
+////////////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////////////
+
 function group(fnName: string, callback: (group: Group) => void) {
   test.group(`LazyChunks.${fnName}`, (group) => {
     group.tap((test) =>
@@ -12,32 +16,60 @@ function group(fnName: string, callback: (group: Group) => void) {
     callback(group);
   });
 }
+
+function createStream(): Stream.PassThrough {
+  return new Stream.PassThrough({
+    highWaterMark: undefined,
+    encoding: "utf8",
+    objectMode: false,
+  });
+}
+
+function createLazyChunks(stream: Stream.PassThrough): LazeChunks {
+  return new LazeChunks(new PullStream(stream, Assert.isBuffer));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////////////
+
 group("getChunk", () => {
   test("can fetch a chunk from start", async ({ expect }) => {
-    const r = new Stream.PassThrough({
-      highWaterMark: undefined,
-      encoding: "utf8",
-      objectMode: false,
-    });
+    const r = createStream();
     r.push("Hello, world!");
     r.push(null);
-    const lazy = new LazeChunks(new PullStream(r, Assert.isBuffer));
+    const lazy = createLazyChunks(r);
 
     const res = await lazy.getChunk(0, 1);
     expect(res).toStrictEqual(["Hello, world!", 0]);
   });
 
   test("can fetch a chunk from offset", async ({ expect }) => {
-    const r = new Stream.PassThrough({
-      highWaterMark: undefined,
-      encoding: "utf8",
-      objectMode: false,
-    });
+    const r = createStream();
     r.push("Hello, world!");
     r.push(null);
-    const lazy = new LazeChunks(new PullStream(r, Assert.isBuffer));
+    const lazy = createLazyChunks(r);
 
     const res = await lazy.getChunk(3, 1);
     expect(res).toStrictEqual(["Hello, world!", 0]);
   });
+  test("can fetch when there are multiple chunks", async ({ expect }) => {
+    const r = createStream();
+    const lazy = createLazyChunks(r);
+    let res = await lazy.getChunk(0, 1);
+    let a = "aaaaaaaaaaa";
+    let b = "bbbbbbbbbbb";
+    let c = "ccccccccccc";
+
+    r.push(a);
+    expect(res).toStrictEqual([a, 0]);
+    r.push(b);
+
+    res = await lazy.getChunk(a.length + 1, 1);
+    expect(res).toStrictEqual([a + b, 0]);
+    r.push(c);
+    r.push(null);
+    res = await lazy.getChunk(a.length + b.length + 1, 1);
+    expect(res).toStrictEqual([b + c, a.length]);
+  }).pin();
 });
